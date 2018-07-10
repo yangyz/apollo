2f9ae1a0333bbcf100daa85da6c03acf1f626c76
yangyz <yangyuanzeng@yeah.net>
2018-07-10 18:25:14 +0800

    修改自身配置信息

--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServiceApplication.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServiceApplication.java
@@ -1,14 +1,10 @@
 package com.ctrip.framework.apollo.configservice;
 
-import com.ctrip.framework.apollo.biz.ApolloBizConfig;
-import com.ctrip.framework.apollo.common.ApolloCommonConfig;
-import com.ctrip.framework.apollo.metaservice.ApolloMetaServiceConfig;
-
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.actuate.system.EmbeddedServerPortFileWriter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,13 +12,18 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.ctrip.framework.apollo.biz.ApolloBizConfig;
+import com.ctrip.framework.apollo.common.ApolloCommonConfig;
+import com.ctrip.framework.apollo.metaservice.ApolloMetaServiceConfig;
+
 /**
  * Spring boot application entry point
  *
  * @author Jason Song(song_s@ctrip.com)
  */
 
-@EnableEurekaServer
+//@EnableEurekaServer
+@EnableEurekaClient
 @EnableAspectJAutoProxy
 @EnableAutoConfiguration // (exclude = EurekaClientConfigBean.class)
 @Configuration
